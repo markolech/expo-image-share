f48e9362577ed8f7500bd3f3d89f92a66a3ff3c8
Mark Olech <mark.olech@gmail.com>
2020-01-20 23:12:09 -0500

    Added image picker

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { Image, StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { Image, StyleSheet, TouchableOpacity, Text, View } from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+
 import logo from './assets/logo.png'
 
 export default function App() {
+  let [selectedImage, setSelectedImage] = useState(null)
+
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync()
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required')
+      return
+    }
+
+    let pickerResults = await ImagePicker.launchImageLibraryAsync()
+
+    if (pickerResults.cancelled === true) {
+      return
+    }
+
+    setSelectedImage({ localUri: pickerResults.uri })
+  }
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: selectedImage.localUri }}
+          style={styles.thumbnail}
+        />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
+      <Image
+        source={{ uri: 'https://i.imgur.com/TkIrScD.png' }}
+        style={styles.logo}
+      />
 
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
 
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
-
-      <TouchableOpacity
-        onPress={() => alert('Hello, world!')}
-        style={{ backgroundColor: 'blue' }}>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
-
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -39,9 +73,16 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 20,
-    color: '#fff'
+    color: '#fff',
   },
   button: {
-
-  }
-});
+    backgroundColor: 'blue',
+    padding: 20,
+    borderRadius: 5,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+  },
+})
